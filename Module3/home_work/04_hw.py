e84a790e94e919189e5f6dff81c272854cec3784
ValerySy <110337610+ValerySy@users.noreply.github.com>
2022-08-07 21:12:42 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -1,3 +1,11 @@
+#import random
+#n = int(input('n: '))
+numbers = [2, -5, 8, 9, -25, 25, 4]
+numbers_sqrt = []
+#for i in range(0, n):
+#    numbers.append(random.randint(-100, 100))
+    
+    
 # Дан список, заполненный произвольными целыми числами, получите новый список,
 # элементами которого будут квадратные корни элементов исходного списка,
 # но только если результаты извлечения корня не имеют десятичной части и
@@ -5,3 +13,9 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+for el in numbers:
+    #print(int(int(el) ** 0.5), type(int(el) ** 0.5))
+    if el > 0 and int(el) % int(el) ** 0.5 == 0:
+        numbers_sqrt.append(int(int(el) ** 0.5))
+print(numbers, numbers_sqrt)        
