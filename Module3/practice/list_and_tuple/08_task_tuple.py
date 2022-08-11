5b7664cf2f4cacd20ac6e43a9208db77ccf0c449
ValerySy <110337610+ValerySy@users.noreply.github.com>
2022-08-11 17:00:43 +0300

    Update 08_task_tuple.py

--- Module3/practice/list_and_tuple/08_task_tuple.py
+++ Module3/practice/list_and_tuple/08_task_tuple.py
@@ -4,12 +4,24 @@
 tup1 = (2, 4, -6)
 tup2 = (2, "Василия", -6)
 tup3 = 2, 4, -6
-tup4 = (2)
+#tup4 = (2)
 tup5 = (2,)
 tup6 = 2,
 tup7 = tuple([2, 4, -6, 12])
-tup8 = tuple(2, 7, 8, -5)
+#tup8 = tuple(2, 7, 8, -5)
 tup9 = tuple()
 tup10 = tuple("hello")
 tup11 = ()
-tup12 = 2 and 4
+#tup12 = 2 and 4
+
+print('tup1: ' , type(tup1))
+print('tup2: ' , type(tup2))
+print('tup3: ' , type(tup3))
+#print('tup4: ' , type(tup4))
+print('tup5: ' , type(tup5))
+print('tup6: ' , type(tup6))
+print('tup7: ' , type(tup7))
+print('tup9: ' , type(tup9))
+print('tup10: ' , type(tup10))
+print('tup11: ' , type(tup11))
+#print('tup12: ' , type(tup12))
