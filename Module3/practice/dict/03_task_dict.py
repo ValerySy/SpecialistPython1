95dd7ffb67b99f68698cebb144c5564a950d9745
ValerySy <110337610+ValerySy@users.noreply.github.com>
2022-08-11 15:31:15 +0300

    Update 03_task_dict.py

--- Module3/practice/dict/03_task_dict.py
+++ Module3/practice/dict/03_task_dict.py
@@ -35,20 +35,35 @@ for i in range(len(staff)):
     if staff[i]['salary'] > max:
         max = staff[i]['salary']
         index = i
-print(staff[index]['name'])
-
+print(staff[index]['name'], staff[index]['surname'], sep = " ")
+#---------------------------------------------------------------------
 print("Имя и Фамилию сотрудника с самой низкой зарплатой:")
-
-# TODO: your code here
+min_salary = staff[0]['salary']
+for i in range(len(staff)):
+    if staff[i]['salary'] < min_salary:
+        min_salary = staff[i]['salary']
+for i in range(len(staff)):
+    if staff[i]['salary'] == min_salary:
+        print(staff[i]['name'], staff[i]['surname'], sep=" ")
 
 print("Среднеарифметическую зарплату всех сотрудников")
-
-# TODO: your code here
-
+#----------------------------------------------------------------
+empl_total_salary = 0
+for empl in staff:
+    empl_total_salary += empl['salary']
+print(int(empl_total_salary/len(staff)))
+#----------------------------------------------------------------
 print("Количество однофамильцев в организации")
-
-# TODO: your code here
-
+surname_list = []
+for el in staff:
+    surname_list.append(el['surname'])
+surname_unique_list = set(surname_list)
+surname_dict = {surname : surname_list.count(surname) for surname in surname_unique_list}
+for surname, surname_count in surname_dict.items():
+    if surname_count > 1:
+        print(surname, surname_count)
+#----------------------------------------------------------------
 print("*Список всех сотрудников(Имя и Фамилию) в порядке возрастания их зарплаты")
-# sorted lambda
-# TODO: your code here
+
+for el in sorted(staff, key=lambda salary_value: salary_value['salary']):
+    print(el['name'], el['surname'])
