335317e322f37ef091f742d4de291a86f08e642b
ValerySy <110337610+ValerySy@users.noreply.github.com>
2022-08-11 15:52:13 +0300

    Update 03_task_list.py

--- Module3/practice/list_and_tuple/03_task_list.py
+++ Module3/practice/list_and_tuple/03_task_list.py
@@ -1,4 +1,9 @@
 # Дан список из n элементов, заполненный произвольными целыми числами в диапазоне от -10 до 10.
 # Вывести на экран сумму всех элементов.
 
-# TODO: your code here
+import random
+
+n = int(input('n: '))
+lst = [random.randint(-10, 10) for _ in range(n)]
+print(lst)
+print(sum(lst))
