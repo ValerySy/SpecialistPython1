833d2b69c400e10bd0e3622c2aef0e8c811b9e70
ValerySy <110337610+ValerySy@users.noreply.github.com>
2022-08-12 09:52:22 +0300

    Update [hw]03_task_string.py

--- Module3/home_work/string/[hw]03_task_string.py
+++ Module3/home_work/string/[hw]03_task_string.py
@@ -1,3 +1,11 @@
 # Посчитайте количество согласных букв в данной строке.
 
-text = ...
+text = 'аовпттлфурвыодфлрыовимфри'
+consonants = 'бвгджзйклмнпрстфхцчшщ'
+
+count_consonants = 0
+
+for letter in text:
+    if letter in consonants:
+        count_consonants += 1
+print('Количество согласных букв: ', count_consonants)
