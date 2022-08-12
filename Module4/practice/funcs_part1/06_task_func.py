0026a0827d2106278520682bf623655a920f24c5
ValerySy <110337610+ValerySy@users.noreply.github.com>
2022-08-12 10:37:29 +0300

    Update 06_task_func.py

--- Module4/practice/funcs_part1/06_task_func.py
+++ Module4/practice/funcs_part1/06_task_func.py
@@ -2,7 +2,10 @@
 # находится строго внутри окружности с центром в точке (xc, yc) и радиусом r:
 
 def point_in_circle(x, y, xc, yc, r):
-    # TODO: your code here
-    pass
-
+    if (xc - x) ** 2 + (yc - y) ** 2 < r ** 2:
+        return True
+    else:
+        return False
 # Не забудьте протестировать вашу функцию
+
+print(point_in_circle(0, 0, 6, 6, 2))
