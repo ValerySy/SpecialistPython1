ec03f3588d7530d847d07df0aecb2845f5d1b341
ValerySy <110337610+ValerySy@users.noreply.github.com>
2022-08-12 11:02:37 +0300

    Update 08_task_func.py

--- Module4/practice/funcs_part1/08_task_func.py
+++ Module4/practice/funcs_part1/08_task_func.py
@@ -1,1 +1,22 @@
 # Напишите функцию находящую n-ое число Фибоначчи.
+
+def fib_num(n):
+
+    f1 = 0
+    f2 = 1
+
+    if n == 1:
+        fi = f1
+    elif n == 2:
+        fi = f2
+    elif n > 2:
+        i = 3
+        fi = 0
+        while i <= n:
+            fi = f1 + f2
+            f1 = f2
+            f2 = fi
+            i += 1
+    return fi
+
+print(fib_num(4))
