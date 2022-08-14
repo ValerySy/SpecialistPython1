4c8aa66cb03ed06e46b0ba3b40221846287add33
ValerySy <110337610+ValerySy@users.noreply.github.com>
2022-08-14 11:26:20 +0300

    Update 04_hw_func.py

--- Module4/home_work/04_hw_func.py
+++ Module4/home_work/04_hw_func.py
@@ -26,3 +26,52 @@ else:
         print(f'{-n if x<0 else n} {p}/{y}')
     else:
         print(f'{x}/{y}')
+#---------------------------------------------------------
+
+def drob(chelya, chislitel, znamenatel):
+    chislitel = chislitel + chelya * znamenatel
+    return chislitel, znamenatel
+def plus(chislitel1, znamenatel1, chislitel2, znamenatel2):
+    return chislitel1 * znamenatel2\
+           + chislitel2 * znamenatel1,\
+           znamenatel1 * znamenatel2
+def minus(chislitel1, znamenatel1, chislitel2, znamenatel2):
+    return chislitel1 * znamenatel2\
+           - chislitel2 * znamenatel1,\
+           znamenatel1 * znamenatel2
+# 111 111/3 + -
+stroka = '1 1/3 + 2 + 1/4'
+lst = stroka.split(' ')
+chislitel1 = None
+znamenatel1 = None
+chislitel2 = None
+znamenatel2 = None
+for i in range(len(lst)-1):
+    if not '/' in lst[i] and not '+' in lst[i] and not '-' in lst[i]  and not '!' in lst[i]:
+        if '/' in lst[i+1]:
+            temp_str = lst[i+1].split('/')
+            chislitel1, znamenatel1 = drob(int(lst[i]),
+                                           int(temp_str[0]), int(temp_str[1]))
+            lst[i] = f'{chislitel1}/{znamenatel1}'
+            lst[i + 1] = '!'
+        else:
+            chislitel1 = lst[i]
+            znamenatel1 = 1
+            lst[i] = f'{chislitel1}/{znamenatel1}'
+lst2 = []
+for i in range(len(lst)):
+    if lst[i] != '!':
+        lst2.append(lst[i])
+print(lst2)
+for i in range(1, len(lst2)-1):
+    if lst2[i] == '+':
+        temp_str1 = lst2[i - 1].split('/')
+        temp_str2 = lst2[i + 1].split('/')
+        x1, x2 = plus(int(temp_str1[0]), int(temp_str1[1]), int(temp_str2[0]), int(temp_str2[1]))
+        lst2[i+1] = f'{x1}/{x2}'
+    elif lst2[i] == '-':
+        temp_str1 = lst2[i - 1].split('/')
+        temp_str2 = lst2[i + 1].split('/')
+        x1, x2 = minus(int(temp_str1[0]), int(temp_str1[1]), int(temp_str2[0]), int(temp_str2[1]))
+        lst2[i+1] = f'{x1}/{x2}'
+print(lst2[-1])
