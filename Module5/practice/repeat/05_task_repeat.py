074660d08e4ce1cd44f57c4e54c7f171d58079a9
ValerySy <110337610+ValerySy@users.noreply.github.com>
2022-08-16 07:32:39 +0300

    Update 05_task_repeat.py

--- Module5/practice/repeat/05_task_repeat.py
+++ Module5/practice/repeat/05_task_repeat.py
@@ -1,3 +1,11 @@
 # Дано общее количество объектов для отображения на веб-сайте и количество объектов на каждой странице.
 # Вычислите, сколько объектов будет отображаться на последней странице сайта.
 
+def last_sheet(num_items, items_on_page):
+    if num_items % items_on_page:
+        return num_items % items_on_page
+    else:
+        return items_on_page
+
+
+print(last_sheet(13356,342))
