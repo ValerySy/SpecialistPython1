08a6877eedb663eee58e5be4892456560d415195
ValerySy <110337610+ValerySy@users.noreply.github.com>
2022-08-16 07:50:54 +0300

    Update 06_task_repeat.py

--- Module5/practice/repeat/06_task_repeat.py
+++ Module5/practice/repeat/06_task_repeat.py
@@ -6,5 +6,9 @@
 # которые не делятся нацело на 400, високосными не являются.
 
 def days_in_year(num_year):
-    pass
+    if (num_year % 4 == 0 and num_year % 100 != 0) or num_year % 400 == 0:
+        return 366
+    else:
+        return 365
 
+print(days_in_year(2021))
