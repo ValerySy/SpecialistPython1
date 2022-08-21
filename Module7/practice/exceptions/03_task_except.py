01bdd529903ce89acf4309d016baeeb4691581a3
ValerySy <110337610+ValerySy@users.noreply.github.com>
2022-08-21 12:58:28 +0300

    Update 03_task_except.py
    Check

--- Module7/practice/exceptions/03_task_except.py
+++ Module7/practice/exceptions/03_task_except.py
@@ -5,3 +5,19 @@
 
 # При решении задачи требуется учесть формат входных данных.
 # Если входные данные некорректные, сообщить об этом.
+
+
+is_correct_string = False
+while not is_correct_string:
+    str_1 = input('input string: ')
+    try:
+        str_list = set(str_1.split(' '))
+        for i in set(str_1.split(' ')):
+            if int(i) > 0:
+                print(i)
+                break
+
+        is_correct_string = True
+    except BaseException as err:
+        print(err)
+
